3815ced43ef8e04141ac4d1f33b926cc7e257404
seongdonghun <162541362+seongdonghun@users.noreply.github.com>
2024-05-08 02:35:19 +0000

    10주차 실습 끝

--- main.js
+++ main.js
@@ -5,6 +5,9 @@
 const port = 3000,
   layouts = require("express-ejs-layouts"), // Listing 12.7 (p. 179)
   express = require("express"),
+  homeController = require("./controllers/homeController"),
+  subscribersController = require("./controllers/subscribersController"),
+  errorController = require("./controllers/errorController"),
   app = express();
 
 /**
@@ -14,7 +17,17 @@ const port = 3000,
  * 애플리케이션에 Mongoose 설정
  * ========================================
  */
-const mongoose = "";
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://ut-node:<password>@ut-node.nhroaly.mongodb.net/?retryWrites=true&w=majority&appName=ut-node",
+  { useNewUrlParser: true }
+);
+
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("db is connect!");
+});
 
 app.set("port", process.env.PORT || port);
 
@@ -28,9 +41,6 @@ app.use(express.static("public")); // 제목 12.5 (p. 181)
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// homeController.js
-const homeController = require("./controllers/homeController");
-
 // Listing 12.6 (p. 178)
 app.get("/", homeController.showHome);
 app.get("/transportation", homeController.showTransportation);
@@ -42,11 +52,9 @@ app.get("/transportation", homeController.showTransportation);
  * 구독자 페이지를 위한 라우트 추가나 바꾸기
  * ========================================
  */
-app.get("/contact", homeController.showSignUp);
-app.post("/contact", homeController.postedSignUpForm);
-
-// errorController.js
-const errorController = require("./controllers/errorController");
+app.get("/contact", subscribersController.getSubscriptionPage);
+app.post("/contact", subscribersController.saveSubscriber);
+app.get("/subscribers", subscribersController.getAllSubscribers);
 
 // Listing 12.12 (p. 184)
 app.use(errorController.logErrors);
